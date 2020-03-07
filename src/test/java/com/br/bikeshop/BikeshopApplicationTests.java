149530c20a9cca066bae3ed95359aa3618707d2d
gribeiros <gustavors071998@gmsil.com>
2020-03-07 17:05:14 -0300

    first commit

---
+++ src/test/java/com/br/bikeshop/BikeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bikeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
