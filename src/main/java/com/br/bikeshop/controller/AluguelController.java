8a978e61ff407dc880f24e2381a91d0a64f0875e
gribeiros <gustavors1998@gmail.com>
2020-05-07 16:03:46 -0300

    Removido erro do Controller

--- src/main/java/com/br/bikeshop/controller/AluguelController.java
+++ src/main/java/com/br/bikeshop/controller/AluguelController.java
@@ -1,35 +1,14 @@
 package com.br.bikeshop.controller;
 
-import com.br.bikeshop.view.BicicletaSaveAndUpdate;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ResponseBody
 @RequestMapping(path = "/aluguel")
 @Api(value = "Api gerenciamento de alugueis")
 @CrossOrigin(value = "*")
 public interface AluguelController {
-
-    @GetMapping(path = "")
-    @ApiOperation(value = "Retorna uma lista de bicicletas")
-    ResponseEntity alugueis();
-
-    @GetMapping(path = "/{id}")
-    @ApiOperation(value = "Retorna uma bicicleta por ID")
-    ResponseEntity aluguel(@PathVariable Long id);
-
-    @PostMapping(path = "")
-    @ApiOperation(value = "Salva uma nova bicicleta")
-    ResponseEntity saveAluguel(@RequestBody BicicletaSaveAndUpdate bicicletaSaveAndUpdate);
-
-    @DeleteMapping(path = "/{id}")
-    @ApiOperation(value = "Deleta uma bicicleta por ID")
-    ResponseEntity deleteAluguel(@PathVariable Long id);
-
-    @PutMapping(path = "/{id}")
-    @ApiOperation(value = "Atualiza um bicicleta por ID")
-    ResponseEntity updateAluguel(@PathVariable Long id, @RequestBody BicicletaSaveAndUpdate bicicletaSaveAndUpdate);
 
 }
