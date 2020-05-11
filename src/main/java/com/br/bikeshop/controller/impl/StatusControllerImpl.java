68ef18cc1df0c0bedd455cdb43a706b1cdb0bb45
gribeiros <gustavors1998@gmail.com>
2020-05-11 17:16:30 -0300

    Adicionada Api de Status

--- src/main/java/com/br/bikeshop/controller/impl/StatusControllerImpl.java
+++ src/main/java/com/br/bikeshop/controller/impl/StatusControllerImpl.java
@@ -1,8 +1,40 @@
 package com.br.bikeshop.controller.impl;
 
 import com.br.bikeshop.controller.StatusController;
+import com.br.bikeshop.model.Status;
+import com.br.bikeshop.service.StatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class StatusControllerImpl implements StatusController {
+
+    @Autowired
+    StatusService statusService;
+
+    @Override
+    public ResponseEntity status() {
+        return statusService.returnAll();
+    }
+
+    @Override
+    public ResponseEntity statu(Long id) {
+        return statusService.findById(id);
+    }
+
+    @Override
+    public ResponseEntity saveStatu(Status status) {
+        return statusService.saveStatus(status);
+    }
+
+    @Override
+    public ResponseEntity deleteStatu(Long id) {
+        return statusService.deleteStatus(id);
+    }
+
+    @Override
+    public ResponseEntity updateStatu(Status status) {
+        return statusService.updateStatus(status);
+    }
 }
