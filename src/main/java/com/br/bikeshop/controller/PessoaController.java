5092a7bf436a8b4c53e8067af48ba6d4a003505c
gribeiros <45661712+gribeiros@users.noreply.github.com>
2020-05-30 15:11:40 -0300

    Mapeamento da API.

--- src/main/java/com/br/bikeshop/controller/PessoaController.java
+++ src/main/java/com/br/bikeshop/controller/PessoaController.java
@@ -11,18 +11,24 @@ import org.springframework.web.bind.annotation.*;
 @Api(value = "Api gerenciamento de Pessoas")
 @CrossOrigin(value = "*")
 public interface PessoaController {
+
+    @GetMapping(path = "")
     @ApiOperation(value = "Retorna uma lista de pessoas")
     ResponseEntity findAll();
 
+    @GetMapping(path = "/{id}")
     @ApiOperation(value = "Procura uma pessoa pelo id")
     ResponseEntity findById(@PathVariable Long id);
 
+    @PostMapping(path = "")
     @ApiOperation(value = "Salva uma pessoa nova")
     ResponseEntity save(@RequestBody Pessoa pessoa);
 
+    @PutMapping(path = "/{id}")
     @ApiOperation(value = "Atualiza uma pessoa")
     ResponseEntity update(@RequestBody Pessoa pessoa);
 
+    @DeleteMapping(path = "/{id}")
     @ApiOperation(value = "Deleta uma pessoa pelo id")
     ResponseEntity delete(@PathVariable Long id);
 
