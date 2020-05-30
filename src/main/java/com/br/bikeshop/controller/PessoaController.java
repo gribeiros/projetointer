db6ed8f892e81df62efaa22680e9b537b225f7d3
gribeiros <45661712+gribeiros@users.noreply.github.com>
2020-05-30 15:09:57 -0300

    Mapeamento do swagger.

--- src/main/java/com/br/bikeshop/controller/PessoaController.java
+++ src/main/java/com/br/bikeshop/controller/PessoaController.java
@@ -2,6 +2,7 @@ package com.br.bikeshop.controller;
 
 import com.br.bikeshop.model.Pessoa;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,15 +11,19 @@ import org.springframework.web.bind.annotation.*;
 @Api(value = "Api gerenciamento de Pessoas")
 @CrossOrigin(value = "*")
 public interface PessoaController {
-
+    @ApiOperation(value = "Retorna uma lista de pessoas")
     ResponseEntity findAll();
 
+    @ApiOperation(value = "Procura uma pessoa pelo id")
     ResponseEntity findById(@PathVariable Long id);
 
+    @ApiOperation(value = "Salva uma pessoa nova")
     ResponseEntity save(@RequestBody Pessoa pessoa);
 
+    @ApiOperation(value = "Atualiza uma pessoa")
     ResponseEntity update(@RequestBody Pessoa pessoa);
 
+    @ApiOperation(value = "Deleta uma pessoa pelo id")
     ResponseEntity delete(@PathVariable Long id);
 
 }
